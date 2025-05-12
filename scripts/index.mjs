502953428167e25d54211b64d9855e484222ab90
Jose Garcia <jmagarciadiez@gmail.com>
2025-05-12 19:53:24 -0300

    Se elimina codigo no usado.

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,29 +1,5 @@
-import { Form, FormField } from "../components/ui/form.mjs";
-import { Input } from "../components/ui/input.mjs";
-import { Label } from "../components/ui/label.mjs";
-
-const fields = [
-  { name: "nombre", label: "Nombre", type: "text", placeholder: "Tu nombre" },
-  { name: "email", label: "Correo", type: "email", placeholder: "Tu correo" },
-  { name: "edad", label: "Edad", type: "number", placeholder: "Tu edad" },
-];
-
 function Page() {
   const contenedor = document.createElement("div");
-
-  const form = Form({
-    fields: [
-      FormField({
-        label: Label({ label: "Nombre", htmlFor: "nombre" }),
-        controlledInput: Input({ name: "nombre", id: "nombre" }),
-      }),
-    ],
-    onSubmit: (data) => {
-      alert(JSON.stringify(data));
-    },
-  });
-  contenedor.appendChild(form);
-
   return contenedor;
 }
 
