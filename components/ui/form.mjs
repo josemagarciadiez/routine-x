119d25b282b44155462f6cba416520f4dd35602e
Jose Garcia <jmagarciadiez@gmail.com>
2025-05-17 11:56:52 -0300

    Se agrego metodo para disparar el submit del form desde afuera del componente.

--- components/ui/form.mjs
+++ components/ui/form.mjs
@@ -65,6 +65,21 @@ export function Form({ fields, submit, onSubmit }) {
     // 3. Ver funcionalidad de array de campos.
   });
 
+  /**
+   * Metodo para ejecutar el submit del form
+   * desde fuera del componente form.
+   *
+   * @param {Function} submitFn Funcion que se ejecutara luego
+   * de la validacion del form, y luego de la funcion onSubmit pasada
+   * en la creacion del form.
+   * Esta funcion recibe formData como parametro.
+   *
+   */
+  container.handleSubmit = function (submitFn) {
+    container.requestSubmit();
+    submitFn && submitFn();
+  };
+
   // Devuelve el elemento <form> creado y configurado.
   return container;
 }
