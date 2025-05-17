2dce1e9b8d7d8c125170b0955563c83893a88beb
Jose Garcia <jmagarciadiez@gmail.com>
2025-05-17 14:48:14 -0300

    Se agregan comentarios a componente Label

--- components/ui/label.mjs
+++ components/ui/label.mjs
@@ -1,7 +1,13 @@
-export function Label({ label = "", htmlFor = "" }) {
-  const $label = document.createElement("label");
-  $label.className = "label-primitive";
-  $label.textContent = label;
-  $label.setAttribute("for", htmlFor);
-  return $label;
+export function Label({ text = "", htmlFor = "" }) {
+  // Crea un nuevo elemento <label> en el DOM.
+  const container = document.createElement("label");
+  // Setea un atributo para mejor accesibilidad.
+  container.setAttribute("data-slot", "label");
+  // Agrega la clase base 'label-primitive' al <label>.
+  container.className = "label-primitive";
+  // Agrega texto al contenido del <label>
+  container.textContent = text;
+  // Setea un atributo para agrupar con un elemento.
+  container.setAttribute("for", htmlFor);
+  return container;
 }
